2335e27542309a09a64abd07fe82687a48ba961e
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-29 08:12:11 +0300

    Feature: Add to register state general error and loading.

--- app/src/main/java/com/example/carto/registration/presentation/state/RegisterFormUiState.kt
+++ app/src/main/java/com/example/carto/registration/presentation/state/RegisterFormUiState.kt
@@ -3,16 +3,10 @@ package com.example.carto.registration.presentation.state
 import androidx.compose.runtime.Immutable
 
 @Immutable
-data class RegisterFormInput(
-    val value: String = "",
-    val isError: Boolean = false,
-    val errorMessage: String = "",
-)
-
-@Immutable
 data class RegisterFormUiState(
     val fullName: RegisterFormInput = RegisterFormInput(),
     val email: RegisterFormInput = RegisterFormInput(),
     val password: RegisterFormInput = RegisterFormInput(),
-    val isLoading: Boolean = false
+    val isLoading: Boolean = false,
+    val generalErrorMessage: String = "",
 )
