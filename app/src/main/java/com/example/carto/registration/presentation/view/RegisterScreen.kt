ce77af50ce3bc95fd4cd2a05e2dd0609a9f65f8e
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-06-29 07:53:14 +0300

    Feature: Add firebase libraries.

--- app/src/main/java/com/example/carto/registration/presentation/view/RegisterScreen.kt
+++ app/src/main/java/com/example/carto/registration/presentation/view/RegisterScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.compose.rememberLifecycleOwner
 import androidx.lifecycle.lifecycleScope
