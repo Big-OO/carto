565a0c32badf16ebaf319f6301402c6087789724
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-07-02 15:48:08 +0300

    chore: make a datasource to check the success and failure in it rather than here in the repo , change toBrand into toDomain

--- app/src/main/java/com/example/carto/feature/home/data/mappers/BrandMapper.kt
+++ app/src/main/java/com/example/carto/feature/home/data/mappers/BrandMapper.kt
@@ -3,7 +3,7 @@ package com.example.carto.feature.home.data.mappers
 import com.example.carto.feature.home.data.model.SmartCollectionDto
 import com.example.carto.feature.home.domain.model.Brand
 
-fun SmartCollectionDto.toBrand(): Brand {
+fun SmartCollectionDto.toDomain(): Brand {
     return Brand(
         id = this.id,
         name = this.title,
