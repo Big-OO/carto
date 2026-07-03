40febfc78c82fe70a65fad322adf65248c0475d6
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-03 09:54:23 +0300

    refactor: Remove unused Serializable annotations from Screen.

--- app/src/main/java/com/example/carto/navigation/Screen.kt
+++ app/src/main/java/com/example/carto/navigation/Screen.kt
@@ -1,8 +1,5 @@
 package com.example.carto.navigation
 
-import kotlinx.serialization.Serializable
-
-@Serializable
 sealed class Screen(val route: String) {
     data object Home : Screen("home")
     data object Search : Screen("search")
@@ -20,11 +17,8 @@ sealed class Screen(val route: String) {
     data object BrandProducts : Screen("brand_products/{brandName}") {
         fun createRoute(brandName: String) = "brand_products/${android.net.Uri.encode(brandName)}"
     }
-    @Serializable
     data object Addresses : Screen("addresses")
-    @Serializable
     data object NewAddress : Screen("new_address")
-    @Serializable
     data object MapPicker : Screen("map_picker")
 }
 
