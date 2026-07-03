5a2f663d9e6381b65a3f9a8079c55cf4cec67c90
Mohamed Amir <me.mohamed.amir@gmail.com>
2026-07-03 09:57:06 +0300

    feat: Add RequiresPermission annotation to getCurrentLocation.

--- app/src/main/java/com/example/carto/feature/map/data/repository/MapRepositoryImpl.kt
+++ app/src/main/java/com/example/carto/feature/map/data/repository/MapRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.example.carto.feature.map.data.repository
 
+import android.Manifest
+import androidx.annotation.RequiresPermission
 import com.example.carto.feature.map.data.datasource.AndroidGeocodingDataSource
 import com.example.carto.feature.map.data.datasource.LocationDataSource
 import com.example.carto.feature.map.data.datasource.MapboxSearchDataSource
@@ -19,6 +21,7 @@ class MapRepositoryImpl @Inject constructor(
     private val geocodingDataSource: AndroidGeocodingDataSource,
 ) : MapRepository {
 
+    @RequiresPermission(Manifest.permission.ACCESS_FINE_LOCATION)
     override suspend fun getCurrentLocation(): MapResult<MapPoint> {
         return when (val result = locationDataSource.getCurrentLocation()) {
             is MapDataResult.Success -> MapResult.Success(result.data)
