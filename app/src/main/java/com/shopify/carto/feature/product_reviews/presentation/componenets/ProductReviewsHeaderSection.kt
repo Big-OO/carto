453be458662f4074b127c443002b202f495d3366
abdelrahman <abdelrahmankarim7@gmail.com>
2026-07-05 18:41:58 +0300

    feat(product-reviews): create header, summary, and list presentation components

---
+++ app/src/main/java/com/shopify/carto/feature/product_reviews/presentation/componenets/ProductReviewsHeaderSection.kt
@@ -0,0 +1,66 @@
+package com.shopify.carto.feature.product_reviews.presentation.componenets
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ProductReviewsHeaderSection(
+    onBackClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(modifier = modifier.fillMaxWidth()) {
+        CircleIconButton(
+            icon = Icons.Outlined.ArrowBack,
+            onClick = onBackClick,
+            modifier = Modifier.align(Alignment.CenterStart)
+        )
+
+        Text(
+            text = "Reviews",
+            style = MaterialTheme.typography.titleLarge,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+    }
+}
+
+@Composable
+private fun CircleIconButton(
+    icon: ImageVector,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = Color.Black
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+            .size(40.dp)
+            .background(MaterialTheme.colorScheme.surface, CircleShape)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = tint,
+            modifier = Modifier.padding(2.dp)
+        )
+    }
+}
