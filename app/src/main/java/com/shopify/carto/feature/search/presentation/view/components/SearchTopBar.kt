305e36d9a718c881e6b74d7963d81c49d20a9eb4
Mohamed_Ali_Shaltoot <mohamedali3092002@gmail.com>
2026-07-05 16:39:23 +0300

    update(search): apply localized strings to search input and top bar

--- app/src/main/java/com/shopify/carto/feature/search/presentation/view/components/SearchTopBar.kt
+++ app/src/main/java/com/shopify/carto/feature/search/presentation/view/components/SearchTopBar.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -29,7 +30,7 @@ fun SearchTopBar(
         contentAlignment = Alignment.Center
     ) {
         Text(
-            text = "Search",
+            text = stringResource(R.string.searchScreenTitle),
             fontSize = 24.sp,
             fontWeight = FontWeight.SemiBold,
             color = MaterialTheme.colorScheme.onBackground,
